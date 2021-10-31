f6c8a0806486267eeada7d3056530465ea46fa79
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-10-31 21:14:28 +0100

    feat: show hexagon on click on start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [isStarted, setIsStarted] = useState(false);
   return (
     <>
       <Head>
@@ -13,15 +14,21 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.boxContainer}>
-          <div className={styles.box}>
-            <div className={styles.boxChildren}>
-              <p className={styles.description}>
-                Kannst du den Weg über&apos;s Eis wieder finden?
-              </p>
-              <button className={styles.button}>Start</button>
+        <div className={styles.container}>
+          {isStarted ? 
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="260">
+              <polygon className={styles.hexagon} points="60,26 45,52 15,52 0,26 15,0 45,0" ></polygon>
+            </svg>
+          :
+            <div className={styles.box}>
+              <div className={styles.boxChildren}>
+                <p className={styles.description}>
+                  Kannst du den Weg über&apos;s Eis wieder finden?
+                </p>
+                <button className={styles.button} onClick={() => setIsStarted(true)}>Start</button>
+              </div>
             </div>
-          </div>
+          }
         </div>
       </main>
 
