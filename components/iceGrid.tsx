46aed854908685619dd6a9a5ab9b4ac92f076caf
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-11-01 21:15:28 +0100

    feat: add and arrange 3 hexagons into a board pattern

--- components/iceGrid.tsx
+++ components/iceGrid.tsx
@@ -2,8 +2,13 @@ import styles from '../styles/IceGrid.module.css'
 
 export function IceGrid() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="260">
-      <polygon className={styles.hexagon} points="60,26 45,52 15,52 0,26 15,0 45,0"></polygon>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" width="300" height="260">
+      <defs>
+        <polygon className={styles.hexagon} id="hexagon" points="60,26 45,52 15,52 0,26 15,0 45,0"></polygon>
+      </defs>
+      <use xlinkHref="#hexagon" transform="translate(0,0)" />
+      <use xlinkHref="#hexagon" transform="translate(92,0)" />
+      <use xlinkHref="#hexagon" transform="translate(46,26)" />
     </svg>
   );
 } 
