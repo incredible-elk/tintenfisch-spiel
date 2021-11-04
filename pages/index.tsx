16198990c6efb1adba3c0a6c8b53b1e93bc891fc
Stephanie Geuder <37314329+incredible-elk@users.noreply.github.com>
2021-11-04 22:36:03 +0100

    feat: pick random start and end hexagons for path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { IceGrid } from '../components/iceGrid'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-const hexagonList: [number, number][] = [
+type Hexagon = [number, number];
+
+const hexagonList: Hexagon[] = [
   [0,5], [0,6], [0,7], [0,8], [0,9], 
   [1,3], [1,4], [1,5], [1,6], [1,7], [1,8], [1,9], 
   [2,0], [2,1], [2,2], [2,3], [2,4], [2,5], [2,6], [2,7], [2,8], [2,9],
@@ -15,16 +17,35 @@ const hexagonList: [number, number][] = [
   [7,2],
 ];
 
-const testPath: [number, number][] = [
-  [1,6], [1,7],
-  [2,5], [2,6], [2,8], [2,9],
-  [3,0], [3,1], [3,4], 
-  [4,2], [4,3], [4,5],
-  [5,3], [5,4], 
+const startHexagons: Hexagon[] = [
+  [1,9], [2,9], [3,9], [4,9], 
 ];
+
+const endHexagons: Hexagon[] = [
+  [2,0], [3,0], [4,0], [4,1], [5,1], 
+];
+
+type Level = {
+  hexagonList: Hexagon[];
+  startHexagons: Hexagon[];
+  endHexagons: Hexagon[];
+}
+
+const level: Level = {hexagonList, startHexagons, endHexagons};
+
+function generatePath({hexagonList, startHexagons, endHexagons}: Level): Hexagon[] {
+  const startHexagonIndex = Math.floor(Math.random() * startHexagons.length);
+  const startHexagon = startHexagons[startHexagonIndex];
+
+  const endHexagonIndex = Math.floor(Math.random() * endHexagons.length);
+  const endHexagon = endHexagons[endHexagonIndex];
+
+  return [startHexagon, endHexagon];
+}
 
 const Home: NextPage = () => {
   const [isStarted, setIsStarted] = useState(false);
+  const path = useMemo(() => generatePath(level), []);
   return (
     <>
       <Head>
@@ -36,7 +57,7 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <div className={styles.container}>
           {isStarted ? 
-            <IceGrid hexagonList={hexagonList} path={testPath} />
+            <IceGrid hexagonList={hexagonList} path={path} />
           :
             <div className={styles.box}>
               <div className={styles.boxChildren}>
